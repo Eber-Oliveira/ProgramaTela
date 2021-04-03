d6c1fabe7577a9ec3029c134b69bf452038598de
Eber-Oliveira <eber.oliv.costa@gmail.com>
2021-04-02 23:08:44 -0300

    Adicionado os botões Inserir e Cancelar

--- src/view/TelaExemplo.java
+++ src/view/TelaExemplo.java
@@ -18,7 +18,7 @@ public class TelaExemplo extends JFrame {
     private final String TIPOSANGUINEO[] = {"A","B","AB","O"};
     private JLabel lblFatorRh;
     private JComboBox cmbFatorRh;
-    private final String TIPOFRH[] = {"NEGATIVO", "POSITIVO"};
+    private final String TIPOFRH[] = {"+", "-"};
     private JLabel lblCurso;
     private JComboBox cmbCurso;
     private final String NOMECURSO[] = {"ADMINISTRAÇÃO", "DIRETO", "SISTEMAS DE INFORMAÇÃO"};
@@ -26,9 +26,12 @@ public class TelaExemplo extends JFrame {
     private JTextField txtContato;
     private JLabel lblTelefoneContato;
     private JFormattedTextField txtTelefoneContato;
+    private JButton btnInserir;
+    private JButton btnCancelar;
+
 
     public TelaExemplo(){
-        setSize(420,450);
+        setSize(420,370);
         setTitle("Sistema de Cadastro");
         setLocationRelativeTo(null);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -66,6 +69,8 @@ public class TelaExemplo extends JFrame {
         cmbFatorRh = new JComboBox(TIPOFRH);
         cmbCurso = new JComboBox(NOMECURSO);
         txtContato = new JTextField();
+        btnInserir = new JButton("Inserir");
+        btnCancelar = new JButton("Cancelar");
 
 
         lblNome.setBounds(10,10,200,25);
@@ -73,20 +78,22 @@ public class TelaExemplo extends JFrame {
         lblTelefone.setBounds(10,70,200,25);
         lblCPF.setBounds(10,100,200,25);
         lblTipoSanguineo.setBounds(10,130,200,25);
-        lblFatorRh.setBounds(10,160,200,25);
-        lblCurso.setBounds(10,190,200,25);
-        lblContato.setBounds(10,220,200,25);
-        lblTelefoneContato.setBounds(10,250,200,25);
+        lblFatorRh.setBounds(265,130,200,25);
+        lblCurso.setBounds(10,160,200,25);
+        lblContato.setBounds(10,190,200,25);
+        lblTelefoneContato.setBounds(10,220,200,25);
 
         txtNome.setBounds(190,10,200,25);
         txtEndereco.setBounds(190,40,200,25);
         txtTelefone.setBounds(190,70,200,25);
         txtCPF.setBounds(190,100,200,25);
-        cmbTipoSanguineo.setBounds(190,130,50,25);
-        cmbFatorRh.setBounds(190,160,200,25);
-        cmbCurso.setBounds(190,190,200,25);
-        txtContato.setBounds(190,220,200,25);
-        txtTelefoneContato.setBounds(190,250,200,25);
+        cmbTipoSanguineo.setBounds(190,130,70,25);
+        cmbFatorRh.setBounds(320,130,70,25);
+        cmbCurso.setBounds(190,160,200,25);
+        txtContato.setBounds(190,190,200,25);
+        txtTelefoneContato.setBounds(190,220,200,25);
+        btnInserir.setBounds(100,260,100,40);
+        btnCancelar.setBounds(200,260,100,40);
 
         tela.add(lblNome);
         tela.add(txtNome);
@@ -106,6 +113,7 @@ public class TelaExemplo extends JFrame {
         tela.add(txtContato);
         tela.add(lblTelefoneContato);
         tela.add(txtTelefoneContato);
+        tela.add(btnInserir);
+        tela.add(btnCancelar);
     }
-
 }
